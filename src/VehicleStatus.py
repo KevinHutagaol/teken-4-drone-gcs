a2930bacf73817f7cf4ae46e0ac92a6c7f8e6081
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-07-25 20:59:08 +0700

    refactored VehicleStatus class to compose Position class and Attitude class

--- src/VehicleStatus.py
+++ src/VehicleStatus.py
@@ -9,20 +9,44 @@ class FlightMode(Enum):
 
 
 @dataclass
-class VehicleStatus:
-    Heartbeat: bool = False
-    # Vehicle State
-    armed: bool = False
-    in_air: bool = False
-    # Position (X, Y, Z)
+class Position:
     latitude: float = 0.0
     longitude: float = 0.0
     altitude: float = 0.0
-    # Attitude
+
+    def __init__(self, lat, lon, alt):
+        self.latitude = lat
+        self.longitude = lon
+        self.altitude = alt
+
+    # TODO: More stuff that is useful for position vectors
+
+
+@dataclass
+class Attitude:
     roll: float = 0.0
     pitch: float = 0.0
     yaw: float = 0.0
+
+    def __init__(self, roll, pitch, yaw):
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
+
+    # TODO: More stuff that is useful for attitude
+
+
+class VehicleStatus:
+    heartbeat: bool
+    # Vehicle State
+    armed: bool
+    in_air: bool
+    # Position (X, Y, Z)
+    position: Position
+    # Attitude
+    attitude: Attitude
     # Battery
-    battery_voltage: float = 0.0
-    battery_percentage: float = 0.0
-    flight_mode: FlightMode = FlightMode.MANUAL
+    battery_voltage: float
+    battery_percentage: float
+    flight_mode: FlightMode
+
