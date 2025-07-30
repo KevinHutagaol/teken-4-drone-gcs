dee1f9d39f9af7a756c5894b1507bbb575fc63f2
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-07-30 19:19:41 +0700

    started working on map window

--- src/MapDisplay/MapDisplayWindow.py
+++ src/MapDisplay/MapDisplayWindow.py
@@ -1,5 +1,7 @@
 from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5 import QtWebEngineWidgets
+import folium, folium.plugins
 
 
 class MapDisplayWindow(QWidget):
@@ -11,3 +13,49 @@ class MapDisplayWindow(QWidget):
         self.setLayout(layout)
 
         self.setMinimumSize(QSize(640, 480))
+
+        self.folium_map = generate_folium_map()
+
+        map_widget = QtWebEngineWidgets.QWebEngineView()
+        map_widget.setHtml(self.folium_map.get_root().render())
+        # print(self.folium_map.get_root().render())
+        layout.addWidget(map_widget)
+
+
+def generate_folium_map() -> folium.Map:
+    folium_map = folium.Map(location=[-6.200000, 106.816666], zoom_start=13)
+    folium.plugins.MousePosition().add_to(folium_map)
+
+    rt_source = folium.JsCode("""
+    (responseHandler, errorHandler) => {
+        var url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+        fetch(url)
+        .then((response) => {
+            return response.json().then((data) => {
+                var { id, longitude, latitude } = data;
+
+                return {
+                    'type': 'FeatureCollection',
+                    'features': [{
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [longitude, latitude]
+                        },
+                        'properties': {
+                            'id': id
+                        }
+                    }]
+                };
+            })
+        })
+        .then(responseHandler)
+        .catch(errorHandler);
+    }
+    """)
+
+    rt = folium.plugins.Realtime(rt_source, interval=5000)
+    rt.add_to(folium_map)
+
+    return folium_map
